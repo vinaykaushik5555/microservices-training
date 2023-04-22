c4d267e8fc65c4e18f376d94d7e099d567b3b916
vinay kaushik <kumar.vinay64@gmail.com>
2023-04-22 08:56:42 +0530

    to-do-service added

---
+++ to-do-service/src/main/java/com/todo/ToDoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoServiceApplication.class, args);
+	}
+
+}
